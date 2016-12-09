b8138629b10cd40c848b08a9bc054e4acf6253ad
VictorSheep <victor.moutton@gmail.com>
2016-12-09 17:33:12 +0100

    Premier apprentissage de lecture de chiffre sur image

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,26 @@
 // creation du perceptron
-var perceptron = new synaptic.Architect.Perceptron(2,3,1);
+var perceptron = new synaptic.Architect.Perceptron(784,5,10);
 
 // Creation d'un nouveau trainer
 var trainer = new synaptic.Trainer(perceptron);
 
-var trainingSet = [
-  {
-    input: [0,0],
-    output: [1]
-  },
-  {
-    input: [0,1],
-    output: [0]
-  },
-  {
-    input: [1,0],
-    output: [0]
-  },
-  {
-    input: [1,1],
-    output: [1]
-  },
-]
+var set = mnist.set(500, 200);
+var trainingSet = set.training;
+var testSet = set.test;
 
 trainer.train(trainingSet,{
 	rate: .1,
-    iterations: 20000,
-    error: .005,
-    shuffle: true,
-    log: 1000,
-    cost: synaptic.Trainer.cost.CROSS_ENTROPY
+  iterations: 100,
+  error: .2,
+  shuffle: true,
+  log: 10,
+  cost: synaptic.Trainer.cost.CROSS_ENTROPY
 });
 
-var result = perceptron.activate([1,0])[0];
+var result = perceptron.activate(testSet[0].input);
 console.log(result);
+
+for (var i = result.length - 1; i >= 0; i--) {
+  result[i] = Math.round(result[i]);
+}
+console.log(testSet[0].output);
